9cd0206ebc7dc3c31d642104526ce41106de14c8
NahirGutierrez <87736603+NahirGutierrez@users.noreply.github.com>
2021-08-24 18:13:41 -0300

    renombre

--- test/turismo/TestAtraccion.java
+++ test/turismo/TestAtraccion.java
@@ -1,12 +1,10 @@
-package atraccion;
+package turismo;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-
-import turismo.Atraccion;
 
 class TestAtraccion {
 	
