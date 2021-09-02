8ba2350f3eb17c9f81b6a9178c85b7be4b968619
tioliban <gahito_el10@hotmail.com>
2021-09-02 20:15:43 -0300

    Se crearon los archivos con informacion de entrada para la aplicacion y una clase App con el main que se encarga de leer los archivos de entrada

--- src/clases/App.java
+++ src/clases/App.java
@@ -1,9 +1,38 @@
 package clases;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		File archivoUsuarios = null;
+		File archivoAtracciones = null;
+		File archivoPromociones = null;
+		FileReader lectorDeArchivoDeUsuarios = null;
+		FileReader lectorDeArchivoDeAtracciones = null;
+		FileReader lectorDeArchivoDePromociones = null;
+		BufferedReader bufferDelLectorDeArchivoDeUsuarios = null;
+		BufferedReader bufferDelLectorDeArchivoDeAtracciones = null;
+		BufferedReader bufferDelLectorDeArchivoDePromociones = null;
+
+		try {
+			archivoUsuarios = new File("usuarios.sad");
+			archivoAtracciones = new File("atracciones.sad");
+			archivoPromociones = new File("promociones.sad");
+			lectorDeArchivoDeUsuarios = new FileReader(archivoUsuarios);
+			lectorDeArchivoDeAtracciones = new FileReader(archivoAtracciones);
+			lectorDeArchivoDePromociones = new FileReader(archivoPromociones);
+			bufferDelLectorDeArchivoDeUsuarios = new BufferedReader(bufferDelLectorDeArchivoDeUsuarios);
+			bufferDelLectorDeArchivoDeAtracciones = new BufferedReader(bufferDelLectorDeArchivoDeAtracciones);
+			bufferDelLectorDeArchivoDePromociones = new BufferedReader(bufferDelLectorDeArchivoDePromociones);
+		} catch (IOException excepcion) {
+			
+		} finally {
+
+		}
 
 	}
 
