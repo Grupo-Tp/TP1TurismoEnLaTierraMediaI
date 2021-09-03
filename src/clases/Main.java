0750232c2c18489c78e8d518722c05f6a9274634
Enzo Rosso Hausberger <enzo.rosso.hausberger@gmail.com>
2021-09-03 11:55:52 -0300

    Update Main.java
    Se arreglaron los constructores de los objetos de atracción y de las distintas promociones.

--- src/clases/Main.java
+++ src/clases/Main.java
@@ -13,30 +13,32 @@ public class Main {
 		Usuario galadriel = new Usuario("Galadriel", 6, 120, TipoAtraccion.PAISAJE);
 		
 		//atracciones
-		Atraccion bosqueNegro = new Atraccion(TipoAtraccion.AVENTURA, 12, 3, "Bosque Negro", 4);
-		Atraccion mordor = new Atraccion(TipoAtraccion.AVENTURA, 4, 25, "Mordor", 3);
-		Atraccion lothlorien = new Atraccion(TipoAtraccion.DEGUSTACION, 30, 35, "Lothlorien", 1);
-		Atraccion laComarca = new Atraccion(TipoAtraccion.DEGUSTACION, 150, 3, "La Comarcar", 6.5);
-		Atraccion minasTirith = new Atraccion(TipoAtraccion.PAISAJE, 25, 5, "Minas Tirith", 2.5);
-		Atraccion abismoDeHelm = new Atraccion(TipoAtraccion.PAISAJE, 15, 5, "Abismo De Helm", 2);
-		Atraccion erebor = new Atraccion(TipoAtraccion.PAISAJE, 32, 12, "Erebor", 3);
+		Atraccion bosqueNegro = new Atraccion("Bosque Negro", 4.0, 3.0, TipoAtraccion.AVENTURA, 12);
+		Atraccion mordor = new Atraccion("Mordor", 3.0, 25.0, TipoAtraccion.AVENTURA, 4);
+		Atraccion lothlorien = new Atraccion("Lothlorien", 1.0, 35.0, TipoAtraccion.DEGUSTACION, 30);
+		Atraccion laComarca = new Atraccion("La comarca", 6.5, 3.0, TipoAtraccion.DEGUSTACION, 150);
+		Atraccion minasTirith = new Atraccion("Minas Tirith", 2.5, 5.0, TipoAtraccion.PAISAJE, 25);
+		Atraccion abismoDeHelm = new Atraccion("Abismo De Helm", 2.0, 5.0, TipoAtraccion.PAISAJE, 15);
+		Atraccion erebor = new Atraccion("Erebor", 3.0, 12.0, TipoAtraccion.PAISAJE, 32);
+		Atraccion moria = new Atraccion("Moria", 2.0, 10.0, TipoAtraccion.AVENTURA, 6);
 		
 		//promociones
 		List<Atraccion> atraccionesPorc = new ArrayList<Atraccion>();
 		atraccionesPorc.add(bosqueNegro);
 		atraccionesPorc.add(mordor);
-		PromocionPorcentual porcentual = new PromocionPorcentual(atraccionesPorc, "pack aventura", 20);
+		atraccionesPorc.add(moria);
+		PromocionPorcentual porcentual = new PromocionPorcentual( "pack aventura", TipoAtraccion.AVENTURA, atraccionesPorc, 20.0);
 		
 		List<Atraccion> atraccionesAbs = new ArrayList<Atraccion>();
 		atraccionesAbs.add(lothlorien);
 		atraccionesAbs.add(laComarca);
-		PromocionAbsoluta absoluta = new PromocionAbsoluta(atraccionesAbs, "pack degustacion", 2);
+		PromocionAbsoluta absoluta = new PromocionAbsoluta( "pack degustacion", TipoAtraccion.DEGUSTACION, atraccionesAbs, 2.0);
 		
 		List<Atraccion> atraccionesAxB = new ArrayList<Atraccion>();
 		atraccionesAxB.add(erebor);
 		atraccionesAxB.add(minasTirith);
 		atraccionesAxB.add(abismoDeHelm);
-		PromocionAxB axb = new PromocionAxB(atraccionesAxB, "pack paisajes");
+		PromocionAxB axb = new PromocionAxB( "pack paisajes", TipoAtraccion.PAISAJE, atraccionesAxB);
 		
 		//uso
 		// agregar promociones y atracciones al sistema
