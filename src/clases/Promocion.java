efbd6eb33cd16a1f87c778ad82221410b3f43111
tioliban <gahito_el10@hotmail.com>
2021-09-04 23:29:22 -0300

    Se corrigio el constructor de la clase y los modificadores de acceso de los metodos

--- src/clases/Promocion.java
+++ src/clases/Promocion.java
@@ -5,25 +5,25 @@ import java.util.List;
 public abstract class Promocion extends Base {
 	private List<Atraccion> atracciones;
 
-	public Promocion(TipoAtraccion tipo, List<Atraccion> atracciones) {
-		super("", 0, 0, tipo);
+	public Promocion(String nombre, double tiempo, double costo, TipoAtraccion tipo, List<Atraccion> atracciones) {
+		super(nombre, tiempo, costo, tipo);
 		this.setAtracciones(atracciones);
 	}
 
 	/**
 	 * @pre No Tiene.
-	 * @post Se suguirio una promocion posibles para un usuario determinado.
+	 * @post Se suguirio una promocion posible para un usuario determinado.
 	 * @param usuario Usuario para el cual se crea la promocion determinada.
 	 * @return Una Promocion para un Usuario.
 	 */
-	public abstract Base sugerirPromocion(Usuario usuario);
+	protected abstract Base sugerirPromocion(Usuario usuario);
 
 	/**
 	 * @pre No tiene.
 	 * @post No tiene.
 	 * @return Lista con las atracciones que incluye la promocion.
 	 */
-	public List<Atraccion> getAtracciones() {
+	protected List<Atraccion> getAtracciones() {
 		return atracciones;
 	}
 
@@ -33,19 +33,7 @@ public abstract class Promocion extends Base {
 	 * @param atracciones Lista con atracciones disponibles.
 	 * @return No tiene.
 	 */
-	public void setAtracciones(List<Atraccion> atracciones) {
+	private void setAtracciones(List<Atraccion> atracciones) {
 		this.atracciones = atracciones;
 	}
-
-	/**
-	 * @pre No Tiene.
-	 * @post Se agrego una atraccion a la lista de atracciones disponibles.
-	 * @param atraccion Atraccion disponible para ser usada en una promocion o
-	 *                  sugerencia.
-	 * @return No tiene.
-	 */
-	public void añadirAtraccion(Atraccion atraccion) {
-		this.atracciones.add(atraccion);
-	}
-
 }
