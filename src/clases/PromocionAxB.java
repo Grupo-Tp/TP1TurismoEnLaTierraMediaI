3cddbe4857a7ba5eef46fdd8aa9968d1861316c7
tioliban <gahito_el10@hotmail.com>
2021-09-04 23:32:37 -0300

    Se modifico el constructor y agregaron metodos que validan los valores que son asignados a sus operaciones

--- src/clases/PromocionAxB.java
+++ src/clases/PromocionAxB.java
@@ -3,13 +3,43 @@ package clases;
 import java.util.List;
 
 public class PromocionAxB extends Promocion {
+	private Atraccion atraccionGratis;
 
-	public PromocionAxB(TipoAtraccion tipo, List<Atraccion> atracciones) {
-		super(tipo, atracciones);
+	public PromocionAxB(String nombre, TipoAtraccion tipo, List<Atraccion> atracciones, Atraccion atraccionGratis) {
+		super(nombre, PromocionPorcentual.calcularTiempo(atracciones), calcularCosto(atracciones), tipo, atracciones);
+		this.setAtraccionGratis(atraccionGratis);
 	}
 
-	public PromocionAxB() {
-		this(null, null);
+	/**
+	 * @pre No tiene.
+	 * @post Se calculo el costo de todas las atracciones.
+	 * @param atracciones Lista de atracciones que incluye la promocion.
+	 * @return Costo total de todas las atracciones.
+	 */
+	protected static double calcularCosto(List<Atraccion> atracciones) {
+		double costo = 0;
+		for (Atraccion atraccion : atracciones) {
+			costo += atraccion.getCosto();
+		}
+		return costo;
+	}
+
+	/**
+	 * @pre
+	 * @post
+	 * @return La atraccion que la promocion ofrece gratis.
+	 */
+	public Atraccion getAtraccionGratis() {
+		return atraccionGratis;
+	}
+
+	/**
+	 * @pre
+	 * @post
+	 * @param atraccionGratis Atraccion que ofrece gratuita la promocion.
+	 */
+	private void setAtraccionGratis(Atraccion atraccionGratis) {
+		this.atraccionGratis = atraccionGratis;
 	}
 
 	@Override
@@ -17,5 +47,4 @@ public class PromocionAxB extends Promocion {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
 }
--- src/clases/PromocionPorcentual.java
+++ src/clases/PromocionPorcentual.java
@@ -6,13 +6,53 @@ public class PromocionPorcentual extends Promocion {
 
 	double porcentajeDescuento;
 
-	public PromocionPorcentual(TipoAtraccion tipo, List<Atraccion> atracciones, double porcentaje) {
-		super(tipo, atracciones);
-		this.porcentajeDescuento = porcentaje;
+	public PromocionPorcentual(String nombre, TipoAtraccion tipo, List<Atraccion> atracciones, double porcentaje) {
+		super(nombre, calcularTiempo(atracciones), calcularCosto(atracciones, porcentaje), tipo, atracciones);
+		this.setPorcentajeDescuento(porcentaje);
 	}
 
-	public PromocionPorcentual(double porcentaje) {
-		this(null, null, porcentaje);
+	/**
+	 * @pre No tiene.
+	 * @post Se calculo el costo de todas las atracciones y aplico el descuento.
+	 * @param atracciones Lista de atracciones que incluye la promocion.
+	 * @return Costo total de todas las atracciones con el porcentaje de descuento
+	 *         aplicado.
+	 */
+	private static double calcularCosto(List<Atraccion> atracciones, double porcentaje) {
+		double costo = 0;
+		for (Atraccion atraccion : atracciones) {
+			costo += atraccion.getCosto();
+		}
+		return costo * validarPorcentaje(porcentaje);
+	}
+
+	/**
+	 * @pre No tiene.
+	 * @post Se valido que el porcentaje de descuento.
+	 * @param porcentaje Valor que de descuento que tiene la promocion.
+	 * @return El porcentaje de descuento de la promocion.
+	 */
+	private static double validarPorcentaje(double porcentaje) {
+		if ((porcentaje > 0) && (porcentaje < 100))
+			return (1 + (porcentaje / 100));
+		else {
+			System.out.println(""); // informar
+			return 0;
+		}
+	}
+
+	/**
+	 * @pre No tiene.
+	 * @post Se calculo la duracion de todas las atracciones.
+	 * @param atracciones Lista de atracciones que incluye la promocion.
+	 * @return Duracion total de todas las atracciones que incluye la promocion.
+	 */
+	protected static double calcularTiempo(List<Atraccion> atracciones) {
+		double tiempo = 0;
+		for (Atraccion atraccion : atracciones) {
+			tiempo += atraccion.getTiempo();
+		}
+		return tiempo;
 	}
 
 	/**
@@ -22,6 +62,17 @@ public class PromocionPorcentual extends Promocion {
 	 */
 	public double getPorcentajeDescuento() {
 		return porcentajeDescuento;
+	}
+
+	/**
+	 * @pre No Tiene.
+	 * @post Se actualizo el porcentaje de descuento del costo de las atracciones
+	 *       contenidas en la promocion.
+	 * @param porcentaje Monto con el porcentaje de descuento.
+	 * @return No tiene.
+	 */
+	private void setPorcentajeDescuento(double porcentaje) {
+		this.porcentajeDescuento = porcentaje;
 	}
 
 	@Override
