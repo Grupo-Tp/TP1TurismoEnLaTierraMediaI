e1010efd677c41cf32ab878c48847f92dc64259a
tioliban <gahito_el10@hotmail.com>
2021-09-04 23:31:55 -0300

    Se modifico el constructor, se agregaron metodos que validan los parametros y asignan los valores de los atributos

--- src/clases/PromocionAbsoluta.java
+++ src/clases/PromocionAbsoluta.java
@@ -6,13 +6,21 @@ public class PromocionAbsoluta extends Promocion {
 
 	double descuentoAbsoluto;
 
-	public PromocionAbsoluta(TipoAtraccion tipo, List<Atraccion> atracciones, double descuentoAbsoluto) {
-		super(tipo, atracciones);
-		this.descuentoAbsoluto = descuentoAbsoluto;
+	public PromocionAbsoluta(String nombre, TipoAtraccion tipo, List<Atraccion> atracciones, double descuentoAbsoluto) {
+		super(nombre, PromocionPorcentual.calcularTiempo(atracciones), PromocionAxB.calcularCosto(atracciones), tipo,
+				atracciones);
+		this.setDescuentoAbsoluto(descuentoAbsoluto);
 	}
 
-	public PromocionAbsoluta(double descuentoAbsoluto) {
-		this(null, null, descuentoAbsoluto);
+	public double getDescuentoAbsoluto() {
+		return descuentoAbsoluto;
+	}
+
+	private void setDescuentoAbsoluto(double descuentoAbsoluto) {
+		if(descuentoAbsoluto<0)
+			this.descuentoAbsoluto = descuentoAbsoluto;
+		else
+			System.out.println(""); // informar que el valor absoluto es negativo
 	}
 
 	@Override
