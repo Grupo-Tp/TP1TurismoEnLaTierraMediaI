f22d3740ef046671db53ad209aefe21e6b3d5efc
tioliban <gahito_el10@hotmail.com>
2021-09-07 19:44:12 -0300

    Se modifican los test

--- src/tests/TestUsuario.java
+++ src/tests/TestUsuario.java
@@ -35,6 +35,14 @@ public class TestUsuario {
 		nombreInvalido = new Usuario("", 6, 120, TipoAtraccion.PAISAJE);
 	}
 
+	@Test
+	public void testGetTiempo() {
+		assertEquals(8, eowyn.getTiempo(), 0);
+		assertEquals(5, gandalf.getTiempo(), 0);
+		assertEquals(8, sam.getTiempo(), 0);
+		assertEquals(6, galadriel.getTiempo(), 0);
+	}
+
 	@Test(expected = ExcepcionDeUsuario.class)
 	public void testDeValidacionDeTiempoNegativo() throws ExcepcionDeUsuario {
 		tiempoInvalido = new Usuario("Galadriel", -6, 120, TipoAtraccion.PAISAJE);
@@ -44,6 +52,15 @@ public class TestUsuario {
 	public void testDeValidacionDeTiempoCero() throws ExcepcionDeUsuario {
 		tiempoInvalido = new Usuario("Galadriel", 0, 120, TipoAtraccion.PAISAJE);
 	}
+
+	@Test
+	public void testGetPresupuesto() {
+		assertEquals(10, eowyn.getPresupuesto(), 0);
+		assertEquals(100, gandalf.getPresupuesto(), 0);
+		assertEquals(36, sam.getPresupuesto(), 0);
+		assertEquals(120, galadriel.getPresupuesto(), 0);
+	}
+
 	@Test(expected = ExcepcionDeUsuario.class)
 	public void testDeValidacionDePresupuestoNegativo() throws ExcepcionDeUsuario {
 		presupuestoInvalido = new Usuario("Galadriel", 6, -120, TipoAtraccion.PAISAJE);
@@ -52,5 +69,13 @@ public class TestUsuario {
 	@Test(expected = ExcepcionDeUsuario.class)
 	public void testDeValidacionDePresupuestoCero() throws ExcepcionDeUsuario {
 		presupuestoInvalido = new Usuario("Galadriel", 6, 0, TipoAtraccion.PAISAJE);
+	}
+
+	@Test
+	public void testGetPreferencia() {
+		assertEquals(TipoAtraccion.AVENTURA, eowyn.getPreferencia());
+		assertEquals(TipoAtraccion.PAISAJE, gandalf.getPreferencia());
+		assertEquals(TipoAtraccion.DEGUSTACION, sam.getPreferencia());
+		assertEquals(TipoAtraccion.PAISAJE, galadriel.getPreferencia());
 	}
 }
