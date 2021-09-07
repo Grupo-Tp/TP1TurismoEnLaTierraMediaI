a16032e1afff229f7ae1c998f38537a97e966549
tioliban <gahito_el10@hotmail.com>
2021-09-07 18:30:06 -0300

    Se corrigio las pruebas, incluyendo algunas nuevas

--- src/tests/TestUsuario.java
+++ src/tests/TestUsuario.java
@@ -1,7 +1,6 @@
 package tests;
 
 import static org.junit.Assert.*;
-
 import org.junit.Before;
 import org.junit.Test;
 
@@ -11,10 +10,8 @@ import excepciones.ExcepcionDeUsuario;
 
 public class TestUsuario {
 
-	Usuario eowyn = null;
-	Usuario gandalf = null;
-	Usuario sam = null;
-	Usuario galadriel = null;
+	Usuario eowyn = null, gandalf = null, sam = null, galadriel = null;
+	Usuario nombreInvalido = null, tiempoInvalido = null, presupuestoInvalido = null;
 
 	@Before
 	public void setUp() throws ExcepcionDeUsuario {
@@ -22,6 +19,7 @@ public class TestUsuario {
 		gandalf = new Usuario("Gandalf", 5, 100, TipoAtraccion.PAISAJE);
 		sam = new Usuario("Sam", 8, 36, TipoAtraccion.DEGUSTACION);
 		galadriel = new Usuario("Galadriel", 6, 120, TipoAtraccion.PAISAJE);
+
 	}
 
 	@Test
@@ -32,4 +30,27 @@ public class TestUsuario {
 		assertEquals("Galadriel", galadriel.getNombre());
 	}
 
+	@Test(expected = ExcepcionDeUsuario.class)
+	public void testDeValidacionDeNombre() throws ExcepcionDeUsuario {
+		nombreInvalido = new Usuario("", 6, 120, TipoAtraccion.PAISAJE);
+	}
+
+	@Test(expected = ExcepcionDeUsuario.class)
+	public void testDeValidacionDeTiempoNegativo() throws ExcepcionDeUsuario {
+		tiempoInvalido = new Usuario("Galadriel", -6, 120, TipoAtraccion.PAISAJE);
+	}
+
+	@Test(expected = ExcepcionDeUsuario.class)
+	public void testDeValidacionDeTiempoCero() throws ExcepcionDeUsuario {
+		tiempoInvalido = new Usuario("Galadriel", 0, 120, TipoAtraccion.PAISAJE);
+	}
+	@Test(expected = ExcepcionDeUsuario.class)
+	public void testDeValidacionDePresupuestoNegativo() throws ExcepcionDeUsuario {
+		presupuestoInvalido = new Usuario("Galadriel", 6, -120, TipoAtraccion.PAISAJE);
+	}
+
+	@Test(expected = ExcepcionDeUsuario.class)
+	public void testDeValidacionDePresupuestoCero() throws ExcepcionDeUsuario {
+		presupuestoInvalido = new Usuario("Galadriel", 6, 0, TipoAtraccion.PAISAJE);
+	}
 }
