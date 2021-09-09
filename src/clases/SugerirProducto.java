2640ef756a765db8e6f54db0abba19623da13a0c
tioliban <gahito_el10@hotmail.com>
2021-09-08 22:38:14 -0300

    Se corrigio errores de sintaxis en metodos de la clase

--- src/clases/SugerirProducto.java
+++ src/clases/SugerirProducto.java
@@ -3,36 +3,74 @@ package clases;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SugerirProducto {
-	private List<Usuario> usuarios = new ArrayList<Usuario>();
-	private List<Promocion> promociones = new ArrayList<Promocion>();
-	private List<Atraccion> atracciones = new ArrayList<Atraccion>();
+import excepciones.ExcepcionDeAtraccion;
+import excepciones.ExcepcionDePromocion;
+import excepciones.ExcepcionDeUsuario;
 
-	public SugerirProducto(List<Usuario> losUsuarios, List<Promocion> lasPromociones, List<Atraccion> lasAtracciones) {
-		this.usuarios = losUsuarios;
-		this.promociones = lasPromociones;
-		this.atracciones = lasAtracciones;
+public class SugerirProducto {
+	private List<Usuario> usuarios = null;
+	private List<Promocion> promociones = null;
+	private List<Atraccion> atracciones = null;
+
+	public SugerirProducto(List<Usuario> losUsuarios, List<Promocion> lasPromociones, List<Atraccion> lasAtracciones)
+			throws ExcepcionDeUsuario, ExcepcionDePromocion, ExcepcionDeAtraccion {
+		this.setUsuarios(losUsuarios);
+		this.setPromociones(lasPromociones);
+		this.setAtracciones(lasAtracciones);
 	}
 
 	/**
-	 * @pre No Tiene.
-	 * @post Se suguirio una promocion posible para un usuario determinado.
-	 * @param usuario Usuario para el cual se crea la promocion determinada.
-	 * @return Una Promocion para un Usuario.
+	 * @return the usuarios
 	 */
-	public void sugerirPromocion(Usuario usuario) {
-		List<Atraccion> misAtracciones = new ArrayList<Atraccion>();
-		boolean cupo = true, noLaVisito = true;
-		misAtracciones = this.getAtracciones();
-		double tiempoDeUsusario = usuario.getTiempo();
-		double presupuesto = usuario.getPresupuesto();
-		for (Atraccion miAtraccion : misAtracciones) {
-			cupo = cupo && (miAtraccion.getCupo() >= 1);
-			noLaVisito = noLaVisito && (this.getAtraccionesDeSuItinerario(usuario).contains(miAtraccion));
-			}
-		if ((this.getTiempo() <= tiempoDeUsusario) && (this.getCosto() <= presupuesto) && cupo && noLaVisito)
-			if (usuario.aceptarSugerencia(this))
-				this.subirAtraccion();
+	public List<Usuario> getUsuarios() {
+		return usuarios;
+	}
+
+	/**
+	 * @param usuarios the usuarios to set
+	 * @throws ExcepcionDeUsuario
+	 */
+	public void setUsuarios(List<Usuario> usuarios) throws ExcepcionDeUsuario {
+		if (!usuarios.isEmpty())
+			this.usuarios = usuarios;
+		else
+			throw new ExcepcionDeUsuario("la lista de usuarios esta vacia");
+	}
+
+	/**
+	 * @return the promociones
+	 */
+	public List<Promocion> getPromociones() {
+		return promociones;
+	}
+
+	/**
+	 * @param promociones the promociones to set
+	 * @throws ExcepcionDePromocion
+	 */
+	public void setPromociones(List<Promocion> promociones) throws ExcepcionDePromocion {
+		if (!promociones.isEmpty())
+			this.promociones = promociones;
+		else
+			throw new ExcepcionDePromocion("la lista de promociones esta vacia");
+	}
+
+	/**
+	 * @return the atracciones
+	 */
+	public List<Atraccion> getAtracciones() {
+		return atracciones;
+	}
+
+	/**
+	 * @param atracciones the atracciones to set
+	 * @throws ExcepcionDeAtraccion
+	 */
+	public void setAtracciones(List<Atraccion> atracciones) throws ExcepcionDeAtraccion {
+		if (!atracciones.isEmpty())
+			this.atracciones = atracciones;
+		else
+			throw new ExcepcionDeAtraccion("la lista de atracciones esta vacia");
 	}
 
 	/**
@@ -42,9 +80,9 @@ public class SugerirProducto {
 	 */
 	public List<Atraccion> getAtraccionesDeSuItinerario(Usuario usuario) {
 		List<Atraccion> retorno = new ArrayList<Atraccion>();
-		List<Base> miItinerario = new ArrayList<Base>();
-		miItinerario = usuario.getItinerario();
-		for (Base baseATratar : miItinerario) {
+		List<Base> suItinerario = new ArrayList<Base>();
+		suItinerario = usuario.getItinerario();
+		for (Base baseATratar : suItinerario) {
 			if (baseATratar instanceof Promocion) {
 				Promocion tratarComoPromocion = (Promocion) baseATratar;
 				retorno.addAll(tratarComoPromocion.getAtracciones());
@@ -58,16 +96,36 @@ public class SugerirProducto {
 	}
 
 	/**
+	 * @pre No Tiene.
+	 * @post Se suguirio una promocion posible para un usuario determinado.
+	 * @return Una Promocion para un Usuario.
+	 */
+	public void sugerirPromocion(Usuario usuario, Promocion promocion) {
+		List<Atraccion> misAtracciones = new ArrayList<Atraccion>();
+		boolean cupo = true, noLaVisito = true;
+		misAtracciones = this.getAtracciones();
+		double tiempoDeUsusario = usuario.getTiempo();
+		double presupuesto = usuario.getPresupuesto();
+		for (Atraccion miAtraccion : misAtracciones) {
+			cupo = cupo && (miAtraccion.getCupo() >= 1);
+			noLaVisito = noLaVisito && (this.getAtraccionesDeSuItinerario(usuario).contains(miAtraccion));
+		}
+		if ((promocion.getTiempo() <= tiempoDeUsusario) && (promocion.getCosto() <= presupuesto) && cupo && noLaVisito)
+			if (usuario.aceptarSugerencia(promocion))
+				promocion.subirAtraccion();
+	}
+
+	/**
 	 * @pre No tiene.
 	 * @post Suguiere esta atraccion al usuario si éste cumple con las condiciones
 	 *       impuestas por la consigna.
 	 * @param usuario
 	 */
-	public void sugerirAtraccion(Usuario usuario) {
-		if (this.getTiempo() <= usuario.getTiempo() && this.getCosto() <= usuario.getPresupuesto()
-				&& this.getCupo() >= 1 && usuario.getItinerario().contains(this))
-			if (usuario.aceptarSugerencia(this))
-				this.subirAtraccion();
+	public void sugerirAtraccion(Usuario usuario, Atraccion atraccion) {
+		if (atraccion.getTiempo() <= usuario.getTiempo() && atraccion.getCosto() <= usuario.getPresupuesto()
+				&& atraccion.getCupo() >= 1 && usuario.getItinerario().contains(atraccion))
+			if (usuario.aceptarSugerencia(atraccion))
+				atraccion.subirAtraccion();
 	}
 
 }
